8614acce3bfd45a2f87e36856ef1651673d429f7
wellerson <100451934+well1999@users.noreply.github.com>
2022-10-12 19:03:11 -0300

    Add files via upload

---
+++ asyncviews/views.py
@@ -0,0 +1,11 @@
+import asyncio
+from unittest import async_case
+import httpx 
+
+async_def http_call_async(async);
+ for num in range(1, 6);
+ await asyncio.sleep(1)
+ print(num)
+ async with httpx.asyncClient() as clien;
+ r = await client.get("https://httpbin.org/")
+ print(r)
